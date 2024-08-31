f633db280c25999dfc4aac28506f5608f3366b6e
José Dario Pintor da Silva <dariocomp@gmail.com>
2024-08-31 11:16:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# logica-programacao-java
+ Código das aulas do curso Lógico de Programação em Java
---
+++ src/main/java/br/edu/ifb/aula1/Exemplo1.java
@@ -0,0 +1,25 @@
+/*
+Esse programa foi criado com intuito de demostrar
+o primeiro programa em java e sua estrutura básica. 
+*/
+
+
+//declarando o pacote que a classe pertece
+package br.edu.ifb.aula1;
+
+//importação de bibliotecas 
+import java.lang.Math;
+
+//criando a classe Exemplo1
+public class Exemplo1{
+    
+    //criando o método principal
+    public static void main (String args []){
+        //aqui no método de fato vai o código do meeu programa 
+        
+        //usando um método de saída print para mostrar a mensagem
+        System.out.println("Olá Mundo!");
+        System.out.println("Raiz quadrada de 16: " + Math.sqrt(16));
+        
+    }
+}
